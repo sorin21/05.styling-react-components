f25adae21daba28c9a69359b2f1a433de89e86a4
Sorinel Dragomir <sorinel.dragomir@hub.vauban.ro>
2018-02-19 17:18:26 +0200

    Add Person.css

--- src/App.js
+++ src/App.js
@@ -13,7 +13,17 @@ class App extends Component {
     toggleClicked: 0
   }
   switchNameHandler = (newName) => {
-    console.log('clicked');
+    console.log('clicked switch');
+    // this.state.persons[0].name = "Adam"
+    this.setState({
+      persons: [
+        { id: 'das4', name: newName, age: '25' },
+        { id: 'dsf1', name: 'Max', age: 38 },
+        { id: 'ghfhg5', name: 'Cristina', age: 19 }
+      ]})
+  }
+  switchNameHandler2 = (newName) => {
+    console.log('clicked switch');
     // this.state.persons[0].name = "Adam"
     this.setState({
       persons: [
@@ -24,18 +34,42 @@ class App extends Component {
   }
 
   nameChangeHandler = (event) => {
+    console.log('clicked change');
+    this.setState({
+      persons: [
+        { id: 'das4', name: event.target.value, age: '25' },
+        { id: 'dsf1', name: event.target.value, age: 38 },
+        { id: 'ghfhg5', name: 'Cristina', age: 19 }
+      ]
+    })
+  }
 
+  nameChangeHandler2 = (event) => {
+    this.setState({
+      persons: [
+        { id: 'das4', name: event.target.value, age: '25' },
+        { id: 'dsf1', name: event.target.value, age: 38 },
+        { id: 'ghfhg5', name: 'Cristina', age: 19 }
+      ]
+    })
   }
   render() {
     return (
       <div className="App">
         <Person 
           name={this.state.persons[0].name} 
-          age="25"
-          click={(event) => this.switchNameHandler('Traian')}>My hooby: guitars</Person>
-        <Person name="Elena" age="18"></Person>
-        <Person name="Cris" age="23"></Person>
-        <button onClick={this.switchNameHandler}>Change Name</button>
+          age="25">My hooby: guitars</Person>
+        <Person 
+          name={this.state.persons[1].name}
+          age="18"
+          click={(event) => this.switchNameHandler('Traian')}
+          changed={this.nameChangeHandler}></Person>
+        <Person 
+          name="Cris" 
+          age="23"
+          click2={(event) => this.switchNameHandler2('dsadsadsadswitch')}></Person>
+        <button onClick={this.switchNameHandler.bind(this, 'Magdalena')}>Change Name</button>
+        <button onClick={() => this.switchNameHandler2('Doi')}>Change Name 2</button>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-
+import classes from './Person.css'
 const person = (props) => {
   return (
-    <div>
+    <div className="Person">
       {/* <p>I use  React for {Math.floor(Math.random() * 10)} years !!!</p> */}
       <p onClick={props.click}>I am {props.name} and I am {props.age} old. I love {props.children || 'nothing'}.</p>
+      {/* <p onClick={props.click2}>Daa {props.name} and I am {props.age} old. I love {props.children || 'nothing'}.</p> */}
       {/* {console.log('props', props)} */}
-      <input type="text" onChange={} />
+      <input type="text" onChange={props.changed} value={props.name} />
     </div>
 
   )
