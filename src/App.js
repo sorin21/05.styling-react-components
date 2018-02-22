44c0b0823321eebe08e0fb23e1e1c6b071169435
Sorinel Dragomir <sorinel.dragomir@hub.vauban.ro>
2018-02-22 17:18:23 +0200

    Add npm eject

--- src/App.js
+++ src/App.js
@@ -1,84 +1,81 @@
 import React, { Component } from 'react';
-import classes from './App.css';
-import Person from './Person/Person';
+import classes from "./App.css";
+import Person from "./Person/Person";
+
 class App extends Component {
   state = {
     persons: [
-      { id: 'das4', name: 'Max', age: 25 },
-      { id: 'dsf1', name: 'Sorin', age: 38 },
-      { id: 'ghfhg5', name: 'Cristina', age: 19 }
+      { id: 'das1', name: 'Sorin', age: '25' },
+      { id: 'das2', name: 'Max', age: 38 },
+      { id: 'das3', name: 'Cristina', age: 19 }
     ],
-    showPersons: false
+    showPersons: false,
+    toggleClicked: 0
   }
-  // target is the input where we type 
-  nameChangedHandler = (event, id) => {
-    const personIndex = this.state.persons.findIndex((personInput) => {
-      return personInput.id === id;
+
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(pers => {
+      return pers.id === id;
     });
-    // create a new object, because just copying 
-    // the new obj will point to the same pointer in memory
-    const person = {
-      ...this.state.persons[personIndex]
-    }
-    // update the name
+
+    // get a copy the actual person {id: "das1", name: "Sorin", age: "25"}
+    const person = {...this.state.persons[personIndex]};
+
+    // get the actual input name value
     person.name = event.target.value;
-    // copy again state persons in a new array
+
+    // get a copy the persons with the updated name value
     const persons = [...this.state.persons];
+    
+    // update persons[personIndex] {id: "das1", name: "Sorin", age: "25"} to
+    // actual person {id: "das1", name: "Sorindfgd", age: "25"}
     persons[personIndex] = person;
-    // update the state persons
-    this.setState({ persons: persons })
+    this.setState({ persons: persons });
   }
+
   deletePersonHandler = (personIndex) => {
-    // copy the full persons array using slice
-    // in a new one
-    // const personsArray = this.state.persons.slice();
-    const personsArray = [...this.state.persons];
-    personsArray.splice(personIndex, 1);
-    // update the state with the new array
-    this.setState({ persons: personsArray })
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
   }
+
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({ showPersons: !doesShow });
+    this.setState({showPersons: !doesShow})
   }
+
   render() {
-    let personsList = null;
-    let btnClass = '';
-    if (this.state.showPersons) {
-      personsList = (
+
+    let persons = null; 
+
+    if(this.state.showPersons) {
+      // if the above condition is true
+      persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person
+            return <Person 
               key={person.id}
+              name={person.name} 
+              age={person.age} 
               click={() => this.deletePersonHandler(index)}
-              name={person.name}
-              age={person.age}
-              changed={(event) => this.nameChangedHandler(event, person.id)} />
+              changed={(event) => this.nameChangeHandler(event, person.id)} />
           })}
-        </div>
+        </div> 
       );
-      btnClass = classes.Red;
+
     }
-    // get a string "red bold"
-    // let classes = ['red', 'bold'].join(' ');
-    let assignedClasses = [];
-    if (this.state.persons.length <= 2) {
-      assignedClasses.push(classes.red) // classes = ['red']
-    }
-    if (this.state.persons.length <= 1) {
-      assignedClasses.push(classes.bold) // classes = ['red', 'bold']
-    }
+
     return (
       <div className={classes.App}>
-        <h1>Hi, I am a front end developer</h1>
-        <p className={assignedClasses.join(' ')}>This is really working!</p>
         <button
-          onClick={this.togglePersonsHandler}
-          className="{btnClass}">Switch Name
- </button>
-        {personsList}
+          className={classes.Red}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons} 
       </div>
     );
   }
 }
 export default App;
+
+
